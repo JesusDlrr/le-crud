1f2af553a37e978368cb54e319f1fac415c11a5a
roddelapan <j2001.dlrr@gmail.com>
2023-10-21 15:36:02 -0700

    c

---
+++ clien/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="datos">
+        <label>Nombre: <input type="text"></input></label><br/>
+        <label>Edad: <input type="text"></input></label><br/>
+        <label>País: <input type="text"></input></label><br/>
+        <label>Cargo: <input type="text"></input></label><br/>
+        <label>Años: <input type="text"></input></label><br/>
+        <button>Registrar</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
